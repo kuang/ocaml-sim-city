fc868b0e299639817a3759c8974560d81ffa272f
Justin Kuang <zk65@cornell.edu>
2017-11-15 19:17:10 -0500

    specs for square and gamestate

--- state.mli
+++ state.mli
@@ -1,4 +1,3 @@
-(* [gamestate] is an abstract type representing the state of an adventure. *)
 type resource =
   | Dining of int*int list
   | Lecture of int*int list
@@ -13,6 +12,7 @@ type building_type =
   | Section of int*int
   | Empty
 
+(* [square] is a type representing the current state of an individual game square. It contains information pertaining to what is currently on the square (represented by field btype), and also attributes that are dependent on btype (ex. maintenance_cost, level). Finally, this type also keeps track of "natural" and "static" information about the square itself, such as its x and y coordinates and terrain features. *)
 type square = {
   btype : building_type;
   level : int;
@@ -20,20 +20,20 @@ type square = {
   ycoord : int;
   maintenance_cost : int;
   terrain : terrain;
-  dining_access : bool;
-  lecture_access : bool;
-  power_access : bool
+
 }
 
+(* [gamestate] is a type representing the state of an adventure. It contains all the information necessary to recreate the current state of the game, including overall information (current money, happiness etc) and individual square information (what is current built on each square, resource connections, etc). The overall game information is stored as record fields, while the square information is stored as a 2D array of type square elements. *)
 type gamestate = {
   disaster : disaster option;
+  lose : bool;
+  message : string option; (*possible prompt for the user*)
   money : int;
   tuition : int;
   happiness: int;
   time_passed : int;
   grid : square array array
 }
-
 (* [init_state j] returns the initial state of the game. *)
 val init_state : gamestate
 
