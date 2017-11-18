b6816b2db6280197672115185143bd172fe9a058
Annie Kathryn Barnett <akb222@cornell.edu>
2017-11-17 20:51:00 -0500

    do' function

--- state.ml
+++ state.ml
@@ -1,3 +1,5 @@
+open Command
+
 type building_type = Unbuildable | Dorm | Resource | Connection | Section
 
 type square = {
@@ -15,3 +17,22 @@ type gamestate = {
   happiness: int;
   grid : square array array
 }
+
+let do_build x y b =
+  failwith "Unimplemented"
+
+let do_delete x y =
+  failwith "Unimplemented"
+
+let do_tuition n =
+  failwith "Unimplemented"
+
+let do_time =
+  failwith "Unimplemented"
+
+let do' (c:command) =
+  match c with
+  | Build (x,y,b) -> do_build x y b
+  | Delete (x,y) -> do_delete x y
+  | SetTuition n -> do_tuition n
+  | TimeStep -> do_time
