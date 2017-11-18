6c003ba41c223f831494c9dd512cd89fbc01822a
Annie Kathryn Barnett <akb222@cornell.edu>
2017-11-17 21:04:08 -0500

    added and updated types, tution respelled

--- state.ml
+++ state.ml
@@ -1,5 +1,22 @@
 open Command
 
+type resource =
+  | Dining of (int*int) list
+  | Lecture of (int*int) list
+  | Power of (int*int) list
+
+type terrain = Water | Forest | Clear | Gorges
+
+type disaster = Fire | Blizzard | Prelim
+
+type building_type =
+  | Dorm of (int*int) list
+  | Resource of resource
+  | Road
+  | Pline
+  | Section of int*int
+  | Empty
+
 type building_type = Unbuildable | Dorm | Resource | Connection | Section
 
 type square = {
@@ -8,11 +25,12 @@ type square = {
   xcoord : int;
   ycoord : int;
   maintenance_cost : int;
+  population: int;
 }
 
 type gamestate = {
   money : int;
-  tution : int;
+  tuition : int;
   num_turns : int;
   happiness: int;
   grid : square array array
