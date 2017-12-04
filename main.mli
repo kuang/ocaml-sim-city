6e8e50606e37e5510e54ea5df6ac4f4914f8e7ed
vagrant <vagrant@vm3110-2017fa>
2017-12-03 21:47:07 -0500

    made lablgtk work with other modules

--- main.mli
+++
@@ -1,4 +0,0 @@
-(* The main loop of the server. Takes HTTP requests with a gamestate and a
- * command formatted in JSON and responds with an updated gamestate serialized
- * to JSON. *)
-val main : unit -> unit
